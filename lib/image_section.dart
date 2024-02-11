f0f194b9a41c50741f1828f252ffd8165c38051c
Suleiman Mustapha <suleimamusty@gmail.com>
2024-02-11 03:42:40 +0100

    added image_section component to the project

---
+++ lib/image_section.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CapuccinoImage extends StatelessWidget {
+  const CapuccinoImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.5, top: 20.0),
+      child: Image.asset(
+        "assets/images/coffee_img.jpg",
+        width: 250.0,
+      ),
+    );
+}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:capuccino_app/detail_section.dart';
+import 'package:capuccino_app/image_section.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,8 @@ class MyApp extends StatelessWidget {
       body: const SingleChildScrollView(
         child: Column(
           children: [
-            DetailSection()
+            DetailSection(),
+            CapuccinoImage()
         ],
         )
       ),
